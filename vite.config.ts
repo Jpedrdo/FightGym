08b06bef44ad1f58722c5c447f1224b3c503b28c
Joao Pedro <jpedroteixeiraphrom@gmail.com>
2023-04-23 20:54:57 -0300

    github pages vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 
 export default defineConfig({
+  base: "/FightGym",
   plugins: [react()],
   resolve: {
     alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }],
